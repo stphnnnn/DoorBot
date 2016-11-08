f1a8c84822f54008b85c42e3962f82737d003f40
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2016-11-08 22:16:51 +0000

    Update toiletbot.js

--- server/toiletbot.js
+++ server/toiletbot.js
@@ -4,35 +4,23 @@ var Botkit = require('botkit');
 var express = require('express');
 var app = express();
 
-var malecontroller = Botkit.slackbot();
-var femalecontroller = Botkit.slackbot();
+var controller = Botkit.slackbot({
+  debug: false,
+  retry: 10
+});
 
-var malebot = malecontroller.spawn({
-  token: process.env.MALE
+var malebot = controller.spawn({
+  token: process.env.MALETOKEN
 }).startRTM()
 
-var femalebot = femalecontroller.spawn({
-  token: process.env.FEMALE
-}).startRTM()
-
-app.get('/male/open', function (req, res) {
-  malebot.startRTM();
-  res.send('Male toilet free');
+app.get('/open', function (req, res) {
+  console.log('Toilet free')
+  res.sendStatus(200);
 });
 
-app.get('/male/close', function (req, res) {
-  malebot.closeRTM();
-  res.send('Male toilet busy');
-});
-
-app.get('/female/open', function (req, res) {
-  femalebot.startRTM();
-  res.send('Female toilet free');
-});
-
-app.get('/female/close', function (req, res) {
-  femalebot.closeRTM();
-  res.send('Female toilet busy');
+app.get('/close', function (req, res) {
+  console.log('Toilet busy')
+  res.sendStatus(200);
 });
 
 app.listen(3000);
