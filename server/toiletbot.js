d4d80e9148da5ba1be3cd744b71af0dc723d953a
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2016-11-12 14:18:11 +0000

    Update toiletbot.js

--- server/toiletbot.js
+++ server/toiletbot.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
 var Botkit = require('botkit');
-var express = require('express');
-var app = express();
 
 var controller = Botkit.slackbot({
   debug: false,
@@ -12,15 +10,3 @@ var controller = Botkit.slackbot({
 var malebot = controller.spawn({
   token: process.env.MALETOKEN
 }).startRTM()
-
-app.get('/open', function (req, res) {
-  console.log('Toilet free')
-  res.sendStatus(200);
-});
-
-app.get('/close', function (req, res) {
-  console.log('Toilet busy')
-  res.sendStatus(200);
-});
-
-app.listen(3000);
