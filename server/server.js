ca4a2d50e519e1a3b9ab4a4d837178d88f658e8f
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2016-11-30 11:24:50 +0000

    add presence change detection

--- server/server.js
+++ server/server.js
@@ -2,27 +2,36 @@ require('dotenv').config();
 
 var Botkit = require('botkit');
 
+var malebotID;
+
 var controller = Botkit.slackbot({
   debug: false,
   retry: 10
 });
 
-var bot = controller.spawn({
+var malebot = controller.spawn({
   token: process.env.MALETOKEN
 });
 
 function start_rtm() {
-  bot.startRTM(function(err,bot,payload) {
+  malebot.startRTM(function(err, malebot, payload) {
     if (err) {
       console.log('Failed to start RTM')
       return setTimeout(start_rtm, 60000);
     }
     console.log("RTM started!");
+    malebotID = malebot.identifyBot().id;
   });
 }
 
-controller.on('rtm_close', function(bot, err) {
+controller.on('rtm_close', function(malebot, err) {
   start_rtm();
 });
 
 start_rtm();
+
+controller.on('presence_change', function(malebot, res, err) {
+  if (res.user == malebotID) {
+    console.log(res.presence);
+  }
+});
